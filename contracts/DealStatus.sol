3cf80b66ae0d327cb1aa477b30137c7d2462c3a3
Xiangan He <xiangan9@gmail.com>
2023-06-01 19:20:36 -0400

    feat: DealStatus contract

---
+++ contracts/DealStatus.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+import "./interfaces/IAggregatorOracle.sol";
+
+contract DealStatus {
+
+    IDeltaAggregatorOracle public deltaAggregatorOracle;
+    mapping(bytes => uint64[]) public cidToActiveDealIDs;
+    mapping(bytes => uint64[]) public cidToExpiringDealIDs;
+
+    constructor(address _deltaAggregatorOracle) {
+        deltaAggregatorOracle = IDeltaAggregatorOracle(_deltaAggregatorOracle);
+    }
+
+    // getActiveDeals should return all the _cid's active dealIds
+    function getActiveDeals(bytes memory _cid) external returns (uint64[] memory) {
+        // get all the deal ids for the cid
+        uint64[] memory allDealIDs = deltaAggregatorOracle.getAllDeals(_cid);
+
+        for (uint i = 0; i < allDealIDs.length; i++) {
+            uint64 dealID = allDealIDs[i];
+            // get the deal's expiration epoch
+            MarketTypes.GetDealActivationReturn memory dealActivationStatus = MarketAPI.getDealActivation(dealID);
+            
+            if (dealActivationStatus.terminated == -1 && dealActivationStatus.activated != -1) {
+                cidToActiveDealIDs[_cid].push(dealID);
+            }
+        }
+
+        return cidToActiveDealIDs[_cid];
+    }
+
+    // getExpiringDeals should return all the deals' dealIds if they are expiring within `epochs`
+    function getExpiringDeals(bytes memory _cid) external returns (uint64[] memory) {
+        // the logic is similar to the above, but use this api call: 
+        // https://github.com/Zondax/filecoin-solidity/blob/master/contracts/v0.8/MarketAPI.sol#LL110C9-L110C9
+        uint64[] memory allDealIDs = deltaAggregatorOracle.getAllDeals(_cid);
+
+        for (uint i = 0; i < allDealIDs.length; i++) {
+            uint64 dealID = allDealIDs[i];
+            // get the deal's expiration epoch
+            MarketTypes.GetDealTermReturn memory dealTerm = MarketAPI.getDealTerm(dealID);
+            
+            if (block.timestamp > uint64(dealTerm.end)) {
+                cidToActiveDealIDs[_cid].push(dealID);
+            }
+        }
+
+        return cidToActiveDealIDs[_cid];
+    }
+}
---
+++ contracts/data-segment/ProofTypes.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+// ProofData is a Merkle proof
+struct ProofData {
+    uint64 index;
+    bytes32[] path;
+}
+
+// InclusionPoof is produced by the aggregator (or possibly by the SP)
+struct InclusionProof {
+    // ProofSubtree is proof of inclusion of the client's data segment in the data aggregator's Merkle tree (includes position information)
+    // I.e. a proof that the root node of the subtree containing all the nodes (leafs) of a data segment is contained in CommDA
+    ProofData proofSubtree;
+    // ProofIndex is a proof that an entry for the user's data is contained in the index of the aggregator's deal.
+    // I.e. a proof that the data segment index constructed from the root of the user's data segment subtree is contained in the index of the deal tree.
+    ProofData proofIndex;
+}
+
+// InclusionVerifierData is the information required for verification of the proof and is sourced
+// from the client.
+struct InclusionVerifierData {
+    // Piece Commitment CID to client's data
+    bytes commPc;
+    // SizePc is size of client's data
+    uint64 sizePc;
+}
+
+// InclusionAuxData is required for verification of the proof and needs to be cross-checked with the chain state
+struct InclusionAuxData {
+    // Piece Commitment CID to aggregator's deal
+    bytes commPa;
+    // SizePa is padded size of aggregator's deal
+    uint64 sizePa;
+}
+
+// SegmentDesc is a description of a data segment.
+struct SegmentDesc {
+    bytes32 commDs;
+    uint64 offset;
+    uint64 size;
+    bytes16 checksum;
+}
+
+struct Fr32 {
+    bytes32 value;
+}
