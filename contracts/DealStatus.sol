03d1443312849c34708fc0ba75f215baedfcee30
Xiangan He <xiangan9@gmail.com>
2023-06-06 12:41:58 -0400

    minor fixes

--- contracts/DealStatus.sol
+++ contracts/DealStatus.sol
@@ -8,26 +8,26 @@ import "./interfaces/IAggregatorOracle.sol";
 
 contract DealStatus {
 
-    IAggregatorOracle public deltaAggregatorOracle;
+    IAggregatorOracle public edgeAggregatorOracle;
 
     event ActiveDeals(uint64[] activeDealIDs);
-    event ExpiringDeals(uint64[] activeDealIDs);
+    event ExpiringDeals(uint64[] expiringDealIDs);
 
-    constructor(address _deltaAggregatorOracle) {
-        deltaAggregatorOracle = IAggregatorOracle(_deltaAggregatorOracle);
+    constructor(address _edgeAggregatorOracle) {
+        edgeAggregatorOracle = IAggregatorOracle(_edgeAggregatorOracle);
     }
 
     // getActiveDeals should return all the _cid's active dealIds
     function getActiveDeals(bytes memory _cid) external returns (uint64[] memory activeDealIDs) {
         // get all the deal ids for the cid
-        activeDealIDs = deltaAggregatorOracle.getAllDeals(_cid);
+        activeDealIDs = edgeAggregatorOracle.getAllDeals(_cid);
 
         for (uint i = 0; i < activeDealIDs.length; i++) {
             uint64 dealID = activeDealIDs[i];
             // get the deal's expiration epoch
             MarketTypes.GetDealActivationReturn memory dealActivationStatus = MarketAPI.getDealActivation(dealID);
             
-            if (dealActivationStatus.terminated != -1 && dealActivationStatus.activated == -1) {
+            if (dealActivationStatus.terminated > 0 && dealActivationStatus.activated == -1) {
                 delete activeDealIDs[i];
             }
         }
@@ -39,7 +39,7 @@ contract DealStatus {
     function getExpiringDeals(bytes memory _cid, uint64 epochs) external returns (uint64[] memory expiringDealIDs) {
         // the logic is similar to the above, but use this api call: 
         // https://github.com/Zondax/filecoin-solidity/blob/master/contracts/v0.8/MarketAPI.sol#LL110C9-L110C9
-        expiringDealIDs = deltaAggregatorOracle.getAllDeals(_cid);
+        expiringDealIDs = edgeAggregatorOracle.getAllDeals(_cid);
 
         for (uint i = 0; i < expiringDealIDs.length; i++) {
             uint64 dealID = expiringDealIDs[i];
